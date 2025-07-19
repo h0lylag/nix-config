3b18a832ddac14a3f4e5f801147acc21d583fead
h0ly lag <h0lylag@gravemind.sh>
2025-07-18 22:36:14 -0700

    enable flatpak

--- modules/system-packages.nix
+++ modules/system-packages.nix
@@ -6,6 +6,15 @@ let
 in
 {
 
+  services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
   # turn on some programs and stuff
   # these are nix options which offer some more integration over raw pkg install
   programs.git.enable = true;
