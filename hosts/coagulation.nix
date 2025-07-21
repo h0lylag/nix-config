caf22f4c893e9001766503db3c6ac90340713e01
h0ly lag <h0lylag@gravemind.sh>
2025-07-21 03:02:31 -0700

    remove unused Minecraft server settings and adjust networking parameters

--- hosts/coagulation.nix
+++ hosts/coagulation.nix
@@ -14,8 +14,9 @@
 
   networking.hostName = "coagulation";
   networking.networkmanager.enable = true;
+  networking.enableIPv6 = false;
 
-  # set static ip
+  networking.defaultGateway = "10.1.1.1";
   networking.interfaces.ens18.ipv4.addresses = [
     {
       address = "10.1.1.10";
@@ -23,20 +24,11 @@
     }
   ];
 
-  networking.defaultGateway = "10.1.1.1";
   networking.nameservers = [
     "1.1.1.1"
     "8.8.8.8"
     "10.1.1.1"
   ];
-
-  # minecraft
-  services.minecraft-server = {
-    enable = false;
-    eula = true;
-    jvmOpts = "-Xms6G -Xmx12G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true";
-    dataDir = "/var/lib/minecraft";
-  };
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
@@ -47,13 +39,11 @@
     22
     80
     443
-    25565
   ];
   networking.firewall.allowedUDPPorts = [
     22
     80
     443
-    25565
   ];
 
   system.stateVersion = "24.05";
