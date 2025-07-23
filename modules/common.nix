d50386f05ce0a2ba08d997960eb38ddf554f1972
h0ly lag <h0lylag@gravemind.sh>
2025-07-23 08:54:02 -0700

    Update nh clean.extraArgs to keep files for 7 days and a maximum of 5

--- modules/common.nix
+++ modules/common.nix
@@ -61,7 +61,7 @@
   programs.nh = {
     enable = true;
     clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
+    clean.extraArgs = "--keep-since 7d --keep 5";
     flake = "/home/chris/.nixos-config";
   };
 
