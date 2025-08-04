7bbf1db1e6eefad66bf62e56d2dbce6c156aeb47
h0ly lag <h0lylag@gravemind.sh>
2025-08-03 20:17:42 -0700

    Add binary and source build configurations for jEveAssets

---
+++ pkgs/jeveassets/binary.nix
@@ -0,0 +1,73 @@
+{
+  stdenv,
+  lib,
+  fetchzip,
+  fetchurl,
+  makeWrapper,
+  copyDesktopItems,
+  makeDesktopItem,
+  jre,
+  javaXms ? "512m",
+  javaXmx ? "4g",
+}:
+
+stdenv.mkDerivation rec {
+  pname = "jeveassets";
+  version = "8.0.3";
+
+  src = fetchzip {
+    url = "https://github.com/GoldenGnu/jeveassets/releases/download/jeveassets-${version}/jeveassets-${version}.zip";
+    sha256 = "dxVLvDrTLCtBrldJ/gyYTE8rXOOGNO2PGT61aCg9ZyI=";
+  };
+
+  icon = fetchurl {
+    url = "https://wiki.jeveassets.org/_media/wiki/logo.png";
+    sha256 = "0y3828ssz7v3hw54099wdcfg66cv2jyb67qr1zbf5wxz16b5i264";
+  };
+
+  nativeBuildInputs = [
+    makeWrapper
+    copyDesktopItems
+  ];
+
+  propagatedBuildInputs = [ jre ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/jeveassets
+    cp -r "${src}/." $out/share/jeveassets/
+
+    install -Dm644 "${src}/license.txt" "$out/share/licenses/${pname}/LICENSE"
+    install -Dm644 "${icon}"            "$out/share/icons/hicolor/64x64/apps/jeveassets.png"
+
+    makeWrapper "${jre}/bin/java" "$out/bin/jeveassets" \
+      --run 'if [ "$JEVE_HEADLESS" = "1" ] || [ "$JEVE_HEADLESS" = "true" ]; then export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Djava.awt.headless=true"; fi' \
+      --add-flags   "-Xms${javaXms}" \
+      --add-flags   "-Xmx${javaXmx}" \
+      --add-flags   "-jar $out/share/jeveassets/jeveassets.jar"
+
+    runHook postInstall
+  '';
+
+  desktopItems = [
+    (makeDesktopItem {
+      name = "jeveassets";
+      desktopName = "jEveAssets";
+      comment = "EVE Online Asset Manager";
+      exec = "jeveassets %U";
+      icon = "jeveassets";
+      categories = [ "Utility" ];
+      terminal = false;
+    })
+  ];
+
+  meta = with lib; {
+    description = "jEveAssets — EVE Online Asset Manager";
+    homepage = "https://github.com/GoldenGnu/jeveassets";
+    license = licenses.gpl2;
+    maintainers = [ maintainers.h0lylag ];
+    platforms = platforms.all;
+    sourceProvenance = [ sourceTypes.binaryBytecode ];
+  };
+}
