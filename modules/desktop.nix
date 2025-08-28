b28ab9648b295dd40ecc241c28eac67a57d97b0b
h0ly lag <h0lylag@gravemind.sh>
2025-08-27 22:06:15 -0700

    DayZ tools package definition and update WideVine setting

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -6,7 +6,7 @@ let
   jeveassets = pkgs.callPackage ../pkgs/jeveassets/default.nix { };
   eve-l-preview = pkgs.callPackage ../pkgs/eve-l-preview/default.nix { };
   wine-test = pkgs.callPackage ../pkgs/wine-test/default.nix { };
-  dayz-a2s = (pkgs.callPackage ../pkgs/dayz-tools/default.nix { }).a2s;
+  dayz-tools = pkgs.callPackage ../pkgs/dayz-tools/default.nix { };
 in
 
 {
@@ -86,7 +86,7 @@ in
     })
 
     (chromium.override {
-      enableWideVine = true;
+      enableWideVine = false;
     })
 
     ntfs3g
@@ -119,7 +119,8 @@ in
     teamspeak3
     jeveassets
     eve-l-preview
-    dayz-a2s
+    dayz-tools.dayz-a2s
+    dayz-tools.dayz-xml
   ];
 
 }
