b881eb3c0002ac159fddb55cf2eb2c2c0cbc6712
h0ly lag <h0lylag@gravemind.sh>
2025-08-27 22:06:27 -0700

    Update DayZ tools source revision and add dayz-xml package definition

--- pkgs/dayz-tools/default.nix
+++ pkgs/dayz-tools/default.nix
@@ -8,11 +8,15 @@ let
   # Keep this in one place so you can reuse for other tools
   src = builtins.fetchGit {
     url = "git@github.com:h0lylag/dayz-tools.git";
-    rev = "853713ceee0d171c54fe832cdae0cdf059b40164";
+    rev = "43d8cf15a19b7f082d979b2ef56043f88754e79d";
   };
 in
 {
   dayz-a2s = pkgs.callPackage ./a2s.nix {
+    inherit lib python3 src;
+    stdenvNoCC = pkgs.stdenvNoCC;
+  };
+  dayz-xml = pkgs.callPackage ./xml-validator.nix {
     inherit lib python3 src;
     stdenvNoCC = pkgs.stdenvNoCC;
   };
