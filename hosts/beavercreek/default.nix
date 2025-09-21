a6032ff6c22a55d7a741c4addd0e0e3ac25578e5
h0ly lag <h0lylag@gravemind.sh>
2025-09-21 13:36:12 -0700

    Refactor ZFS and networking configuration in default.nix for beavercreek host

--- hosts/beavercreek/default.nix
+++ hosts/beavercreek/default.nix
@@ -12,10 +12,7 @@
     ../../modules/tailscale.nix
   ];
 
-  networking.hostName = "beavercreek";
-  networking.hostId = "8425e349"; # Required for ZFS
-
-  # ZFS configuration is handled by hardware-configuration.nix and disko.nix
+  # ZFS configuration
   boot.zfs.devNodes = "/dev/disk/by-id";
   services.zfs.autoScrub.enable = true;
 
@@ -23,9 +20,33 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.enable = true;
 
+  # Networking configuration
+  networking.hostName = "beavercreek";
+  networking.hostId = "8425e349"; # Required for ZFS
+  networking.enableIPv6 = false;
+  networking.defaultGateway = "10.1.1.1";
+  networking.interfaces.ens18.ipv4.addresses = [
+    {
+      address = "10.1.1.50";
+      prefixLength = 24;
+    }
+  ];
+
+  networking.nameservers = [
+    "1.1.1.1"
+    "8.8.8.8"
+    "10.1.1.1"
+  ];
+
   # Enable SSH for remote access
   services.openssh.enable = true;
-  security.sudo.enable = true;
+
+  # Firewall Rules
+  networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [
+    22
+  ];
+  networking.firewall.allowedUDPPorts = [ ];
 
   system.stateVersion = "25.05";
 }
