9e6fe5fe31c102b01967dbb22b87b6ec1dbd943f
h0ly lag <h0lylag@gravemind.sh>
2025-09-24 21:30:30 -0700

    podman: clean up comments and remove unused registries and packages

--- modules/podman.nix
+++ modules/podman.nix
@@ -11,30 +11,28 @@
 
   virtualisation.podman = {
     enable = true;
-    # Create a `docker` alias for podman, to use it as a drop-in replacement
+
+    # Create a `docker` alias for podman
     dockerCompat = true;
+
     # Enable DNS for container-to-container name resolution (netavark + aardvark)
     defaultNetwork.settings = {
       dns_enabled = true;
     };
-    # Periodically prune old images/containers
+
     autoPrune = {
       enable = true;
       dates = "weekly";
     };
   };
 
-  # Common registries for image search
   virtualisation.containers.registries.search = [
     "docker.io"
-    "ghcr.io"
-    "quay.io"
   ];
 
-  # Helpful tooling
   environment.systemPackages = with pkgs; [
     podman-compose
-    skopeo
-    dive
+    #skopeo
+    #dive
   ];
 }
