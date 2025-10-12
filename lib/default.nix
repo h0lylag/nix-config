813325edce5ba5666a717331a7c0639cc3502c98
h0ly lag <h0lylag@gravemind.sh>
2025-10-12 09:59:10 -0700

    add Cachix binary caches for gaming inputs in mkWorkstation

--- lib/default.nix
+++ lib/default.nix
@@ -34,6 +34,21 @@
       extraSpecialArgs = {
         inherit (inputs) nix-gaming nix-citizen winapps;
       };
+      extraModules = [
+        # Cachix binary caches for gaming inputs
+        {
+          nix.settings = {
+            substituters = [
+              "https://nix-gaming.cachix.org"
+              "https://nix-citizen.cachix.org"
+            ];
+            trusted-public-keys = [
+              "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+              "nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="
+            ];
+          };
+        }
+      ];
     };
 
   # Standard server host - stable nixpkgs
