deb07fc92ce7f47b17304b622b27b88f113137ea
h0ly lag <h0lylag@gravemind.sh>
2025-11-04 01:38:14 -0800

    fix: update commit reference for prism-django to ensure reproducibility

--- pkgs/prism-django/default.nix
+++ pkgs/prism-django/default.nix
@@ -10,7 +10,7 @@ let
   src = builtins.fetchGit {
     url = "git@github.com:h0lylag/prism-django.git";
     #ref = "main";
-    rev = "c6eed248084eaff55b69b58bec5436464e20d858"; # pin to specific commit for reproducibility
+    rev = "89229ecf809daf9e5a45a27ddcbc7e4959dc97b1"; # pin to specific commit for reproducibility
     # To get the latest commit hash: git ls-remote git@github.com:h0lylag/prism-django.git main
   };
 
