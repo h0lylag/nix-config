82570f2b8c5ac4d5ff16d93f9095d0cb0eb1b010
h0ly lag <h0lylag@gravemind.sh>
2025-11-03 19:41:26 -0800

    fix: set DEBUG to false in environment configuration and remove redundant proxy headers in Nginx

--- hosts/midship/web/nginx.nix
+++ hosts/midship/web/nginx.nix
@@ -50,13 +50,7 @@
       locations."/" = {
         proxyPass = "http://127.0.0.1:8000";
         proxyWebsockets = true;
-        extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
-          proxy_set_header X-Forwarded-Host $host;
-        '';
+        # Note: recommendedProxySettings already sets most headers, don't duplicate
       };
     };
 
