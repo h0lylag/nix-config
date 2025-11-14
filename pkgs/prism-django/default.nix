33fdf2c22a8791ac3bf87de1469fa782814b3e24
h0ly lag <h0lylag@gravemind.sh>
2025-11-13 23:10:39 -0800

    prism-django: update to 55376fe66e71d952fd23267949ad4219fab2c6b2

--- pkgs/prism-django/default.nix
+++ pkgs/prism-django/default.nix
@@ -10,7 +10,7 @@ let
   src = builtins.fetchGit {
     url = "git@github.com:h0lylag/prism-django.git";
     #ref = "main";
-    rev = "8cf32671d1bad0891a862b58be35021760e0cda5"; # pin to specific commit for reproducibility
+    rev = "55376fe66e71d952fd23267949ad4219fab2c6b2"; # pin to specific commit for reproducibility
     # To get the latest commit hash: git ls-remote git@github.com:h0lylag/prism-django.git main
   };
 
