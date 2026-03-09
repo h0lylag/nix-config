85cfbfb0280c591dfb756e36aea06030a08aa02b
h0lylag <h0lylag@gravemind.sh>
2026-03-08 22:01:00 -0700

    fix: add experimental features for nix-command and flakes in base profile

--- profiles/base.nix
+++ profiles/base.nix
@@ -12,6 +12,11 @@
     ../features/tailscale.nix
   ];
 
+  nix.settings.experimental-features = lib.mkDefault [
+    "nix-command"
+    "flakes"
+  ];
+
   # Timezone and locale
   time.timeZone = "America/Los_Angeles";
   i18n.defaultLocale = "en_US.UTF-8";
