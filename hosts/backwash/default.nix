17b2920b28606b4ef970188837a320932202e2bf
h0lylag <h0lylag@gravemind.sh>
2026-03-09 12:51:24 -0700

    refactor: update backwash configuration and clean up unused services

--- hosts/backwash/default.nix
+++ hosts/backwash/default.nix
@@ -1,11 +1,12 @@
-# backwash - 10.1.1.178
-{ config, pkgs, ... }:
+# backwash - Thinkpad x230 laptop
+{ pkgs, ... }:
 
 {
   imports = [
     ./hardware-configuration.nix
     ../../profiles/base.nix
     ../../profiles/common.nix
+    ../../profiles/workstation.nix
   ];
 
   boot.loader.grub.enable = true;
@@ -13,32 +14,11 @@
   boot.loader.grub.useOSProber = true;
 
   networking.hostName = "backwash";
-  networking.networkmanager.enable = true;
 
-  services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedUDPPorts = [ ... ];
 
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  services.printing.enable = true;
-
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  programs.firefox.enable = true;
-
-  networking.firewall.allowedTCPPorts = [ 22 ];
-
+  # Enable distributed builds to speed up rebuilds on this older laptop
   nix.distributedBuilds = true;
   nix.buildMachines = [
     {
