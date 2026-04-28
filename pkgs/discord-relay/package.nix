6fa65d568cfae1c03fefe0c8973978db764f7bb5
h0lylag <h0lylag@gravemind.sh>
2026-04-27 19:00:20 -0700

    discord-relay: bump to 8134d7ef47a9

--- pkgs/discord-relay/package.nix
+++ pkgs/discord-relay/package.nix
@@ -99,7 +99,7 @@ pkgs.stdenv.mkDerivation {
 
   src = builtins.fetchGit {
     url = "ssh://git@github.com/h0lylag/discord-relay.git";
-    rev = "b69e9c761f1cfbe82f2943c345b5fd372555413a";
+    rev = "8134d7ef47a9c7511cd2cd1088d8e9c6fafbdbc4";
     allRefs = true;
   };
 
