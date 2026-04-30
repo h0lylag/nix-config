ba9f8f81626f765abca67f36c8dac2578f80be06
h0lylag <h0lylag@gravemind.sh>
2026-04-29 22:21:49 -0700

    steak-bot: bump to c522afdb81af

--- pkgs/steak-bot/package.nix
+++ pkgs/steak-bot/package.nix
@@ -23,7 +23,7 @@ pkgs.stdenv.mkDerivation {
   # repo is split.
   src = builtins.fetchGit {
     url = "ssh://git@github.com/h0lylag/discord-relay.git";
-    rev = "da9c5ac91af2cb7fe3cfd16c67e3e8e31218acf8";
+    rev = "c522afdb81af1968c06885a842458d832e13ae4e";
     allRefs = true;
   };
 
