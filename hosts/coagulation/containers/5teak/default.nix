a7a5f0dbefa2f1d42a750906241855b3523cf93f
h0lylag <h0lylag@gravemind.sh>
2026-07-05 12:12:31 -0700

    temp ssh rules

--- hosts/coagulation/containers/5teak/default.nix
+++ hosts/coagulation/containers/5teak/default.nix
@@ -37,6 +37,14 @@
 
         environment.systemPackages = [ pkgs.age ];
 
+        # Temporary migration access. Remove after 5teak service cutover.
+        services.openssh.settings.PermitRootLogin = lib.mkForce "prohibit-password";
+        users.users.root.openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMWU3a+HOcu4woQiuMoCSxrW8g916Z9P05DW8o7cGysH chris@relic"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFB1tzcZzr4LNtG/8uQZ8zTV9QkcmWSL3NGLgmE+oP1D chris@midship"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGgptHWisSxEzg5hSPIN+Rh8M1tWMEgNED0QOvHmWLOy root@midship"
+        ];
+
         networking.interfaces.eth0.useDHCP = false;
         networking.interfaces.eth0.ipv4.addresses = [
           {
