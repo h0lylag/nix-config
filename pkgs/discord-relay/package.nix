5625b124c7f7a6471175ee5282e50273edbb4b69
h0lylag <h0lylag@gravemind.sh>
2026-08-02 23:33:52 -0700

    discord-relay: bump to 6333633ddca0

--- pkgs/discord-relay/package.nix
+++ pkgs/discord-relay/package.nix
@@ -97,11 +97,11 @@ let
 in
 pkgs.stdenv.mkDerivation {
   pname = "discord-relay";
-  version = "unstable-2026-08-03";
+  version = "unstable-2026-08-02";
 
   src = builtins.fetchGit {
     url = "ssh://git@github.com/Outback-Steakhouse-Of-Pancakes/discord-relay.git";
-    rev = "ff125b613b24c9e04fb661ce68df39c568b1c56a";
+    rev = "6333633ddca06378b44d1ff7c78413e021312500";
     allRefs = true;
   };
 
