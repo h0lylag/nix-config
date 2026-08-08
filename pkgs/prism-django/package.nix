fdd8fe34b38966b730a78014dc290616c533f885
h0lylag <h0lylag@gravemind.sh>
2026-08-08 11:09:32 -0700

    prism-django: update to 9b8efbb2364e11d55c3a9fa9c91f36910039a16f

--- pkgs/prism-django/package.nix
+++ pkgs/prism-django/package.nix
@@ -10,7 +10,7 @@ let
   src = builtins.fetchGit {
     url = "git@github.com:Outback-Steakhouse-Of-Pancakes/prism-django.git";
     #ref = "main";
-    rev = "75225ad1d99b2ee739345c3e0efaa8f238554e3c"; # pin to specific commit for reproducibility
+    rev = "9b8efbb2364e11d55c3a9fa9c91f36910039a16f"; # pin to specific commit for reproducibility
     # To get the latest commit hash: git ls-remote git@github.com:Outback-Steakhouse-Of-Pancakes/prism-django.git main
   };
 
