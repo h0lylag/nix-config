aa69d96e3e59f67d911623e5b11b4b32d5e17462
h0lylag <h0lylag@gravemind.sh>
2026-08-25 19:23:17 -0700

    remove volume

--- hosts/midship/default.nix
+++ hosts/midship/default.nix
@@ -27,15 +27,6 @@
   services.openssh.enable = true;
   services.timesyncd.enable = true;
 
-  fileSystems."/mnt/volume-hil-1" = {
-    device = "/dev/disk/by-id/scsi-0HC_Volume_105566128";
-    fsType = "ext4";
-    options = [
-      "nofail"
-      "x-systemd.device-timeout=30s"
-    ];
-  };
-
   swapDevices = [
     {
       device = "/var/lib/swapfile";
