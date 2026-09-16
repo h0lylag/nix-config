2ad501288e43aa9c9086ae2c54e72ed3d11dd01c
h0lylag <h0lylag@gravemind.sh>
2026-09-16 09:45:21 -0700

    drop evebuddy

--- den/aspects/gaming.nix
+++ den/aspects/gaming.nix
@@ -78,7 +78,6 @@
         inputs.eve-preview-manager.packages.${pkgs.stdenv.hostPlatform.system}.default
         inputs.set-desto.packages.${pkgs.stdenv.hostPlatform.system}.default
         # (pkgs.callPackage ../../pkgs/eve-preview-manager/package.nix { })
-        (pkgs.callPackage ../../pkgs/evebuddy/package.nix { })
       ];
     };
 }
